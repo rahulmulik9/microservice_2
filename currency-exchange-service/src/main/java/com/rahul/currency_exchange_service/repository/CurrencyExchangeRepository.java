c9af922acb0ba1bc1c1bfea36ccf6a1149a5cd92
RAHUL <cycv@jhhj/com>
2024-10-27 01:07:00 +0530

    Added repository Added query

---
+++ currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.rahul.currency_exchange_service.repository;
+
+import com.rahul.currency_exchange_service.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
