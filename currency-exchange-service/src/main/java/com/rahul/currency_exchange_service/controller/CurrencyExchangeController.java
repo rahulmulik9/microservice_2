82febae8f22b12c228f5f49da2f8adcbdbc72ad3
RAHUL <cycv@jhhj/com>
2024-10-27 01:11:29 +0530

    Added repository in controller

--- currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.rahul.currency_exchange_service.controller;
 
 import com.rahul.currency_exchange_service.bean.CurrencyExchange;
+import com.rahul.currency_exchange_service.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,23 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
     @Autowired
-    private Environment theEnviroment;
+    private CurrencyExchangeRepository repository;
+    @Autowired
+    private Environment environment;
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange thecurrencyExchange = new CurrencyExchange(1001L, from, to, BigDecimal.valueOf(80));
-        String port = theEnviroment.getProperty("local.server.port");
-        thecurrencyExchange.setEnvironment(port);
-        return thecurrencyExchange;
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if(currencyExchange ==null) {
+            throw new RuntimeException("Unable to Find data for " + from + " to " + to);
+        }
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
     }
 
 }
