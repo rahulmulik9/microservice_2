897db70c92faab69d7a879f3c651b8ebc5e195a1
RAHUL <cycv@jhhj/com>
2024-10-27 01:04:18 +0530

    Added Entity class

--- currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/bean/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/bean/CurrencyExchange.java
@@ -1,13 +1,24 @@
 package com.rahul.currency_exchange_service.bean;
 
 import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal conversionMultiple;
-    private String environment;                  //this is used to save the port
+    private String environment;
 
     public CurrencyExchange() {
 
