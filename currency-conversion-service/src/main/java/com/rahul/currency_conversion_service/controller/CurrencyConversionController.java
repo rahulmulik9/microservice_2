df5ea271f504ff0e517abc365561a461a8ccfd2d
RAHUL <cycv@jhhj/com>
2024-10-27 01:48:56 +0530

    API to get conversion from conversion exchanger

--- currency-conversion-service/src/main/java/com/rahul/currency_conversion_service/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/rahul/currency_conversion_service/controller/CurrencyConversionController.java
@@ -2,9 +2,11 @@ package com.rahul.currency_conversion_service.controller;
 
 
 import com.rahul.currency_conversion_service.bean.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -19,7 +21,23 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal quantity
     ) {
-        return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.valueOf(65), BigDecimal.valueOf(6500), "8001");
 
+       // return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.valueOf(65), BigDecimal.valueOf(6500), "8001");
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+
+        ResponseEntity<CurrencyConversion> responseEntity =
+                new RestTemplate()
+                        .getForEntity("http://localhost:8001/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()+ " " + "rest template");
     }
 }
