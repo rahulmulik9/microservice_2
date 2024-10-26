2939ab0f45feee18667d18fc9d08a3f84bf34249
RAHUL <cycv@jhhj/com>
2024-10-27 00:48:46 +0530

    Added class Environment imported org.springframework.core.env.Environment; this will give server port

--- currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.rahul.currency_exchange_service.controller;
 
 import com.rahul.currency_exchange_service.bean.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,14 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
-
+    @Autowired
+    private Environment theEnviroment;
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
         CurrencyExchange thecurrencyExchange = new CurrencyExchange(1001L, from, to, BigDecimal.valueOf(80));
-        thecurrencyExchange.setEnvironment("8080");
+        String port = theEnviroment.getProperty("local.server.port");
+        thecurrencyExchange.setEnvironment(port);
         return thecurrencyExchange;
     }
 
