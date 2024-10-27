f14f1261a7c05a37bd46debfac174692741cb38c
RAHUL <cycv@jhhj/com>
2024-10-27 14:49:46 +0530

    Added redirect route so that each time no need to add microservice name (application name from eureka)

---
+++ api-gateway/src/main/java/com/rahul/api_gateway/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,44 @@
+package com.rahul.api_gateway.configuration;
+
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator gateWayRoute(RouteLocatorBuilder builder){
+//        Function<PredicateSpec, Buildable<Route>> routeFunction = p->p.path("/get").uri("http://httpbin.org:80");
+//        return builder.routes().route(routeFunction).build();
+        /*in this dont need long url like this
+        * http://localhost:8765/CURRENCY-CONVERSION-SERVICE/currency-conversion-feign/from/USD/to/INR/quantity/80
+        * instead it will redirect if it start with CURRENCY-CONVERSION-SERVICE to currency conversion service */
+
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")      //request coming for currency-exchange
+                        .uri("lb://currency-exchange"))                   //load balance and currency-exchange
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
