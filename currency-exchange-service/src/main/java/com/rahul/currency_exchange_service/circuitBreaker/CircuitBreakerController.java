031a8ddd282057bb6cead006e3c3356a5a4c6a8a
RAHUL <rahulmulik9@gmail.com>
2024-11-01 17:00:12 +0530

    Added rate limit so every 2 secc 10 request will be permitted

--- currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/circuitBreaker/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/circuitBreaker/CircuitBreakerController.java
@@ -2,6 +2,7 @@ package com.rahul.currency_exchange_service.circuitBreaker;
 
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     //@Retry(name="sample-api", fallbackMethod="hardCodedResponse")
     @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name="default")               //this allow only in 10 se only allow 100 request
     public String getSimpleAPI() {
         logger.info("Sample api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",String.class);
