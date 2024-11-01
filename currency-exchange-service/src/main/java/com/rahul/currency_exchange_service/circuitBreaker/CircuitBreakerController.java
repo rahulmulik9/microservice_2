7d663a0aca39785501d575045d242d90bc533b82
RAHUL <rahulmulik9@gmail.com>
2024-11-01 16:32:17 +0530

    Added fall back response

--- currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/circuitBreaker/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/rahul/currency_exchange_service/circuitBreaker/CircuitBreakerController.java
@@ -1,14 +1,29 @@
 package com.rahul.currency_exchange_service.circuitBreaker;
 
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class CircuitBreakerController {
-    @GetMapping("/simpleapi")
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/simple-api")
+    @Retry(name="simple-api", fallbackMethod="hardCodedResponse")
     public String getSimpleAPI() {
-        return "Simple_API";
+        logger.info("Sample api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",String.class);
+       // return "Simple_API";
+        return forEntity.getBody();
+    }
+
+    public String hardCodedResponse(Exception exception){
+        return "Fall-back Repose";
     }
 
 }
